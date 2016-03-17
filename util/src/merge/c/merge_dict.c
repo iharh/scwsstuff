32148d8d377feff3ee9044ac04edf0bcb839dab3
Ihar Hancharenka <iigoncharenko@gmail.com>
2016-03-17 11:23:46 +0300

    minor

--- util/src/merge/c/merge_dict.c
+++ util/src/merge/c/merge_dict.c
@@ -1,7 +1,3 @@
-#ifdef HAVE_CONFIG_H
-#	include "config.h"
-#endif
-
 #include "scws/xtree.h"
 #include "scws/xdb.h"
 #include "scws/xdict.h"
--- util/src/norm/c/norm_dict.c
+++ util/src/norm/c/norm_dict.c
@@ -1,8 +1,122 @@
+#include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+static char *program_name;
+static void show_usage(int code, const char *msg)
+{
+    if (code)
+    {
+        if (msg != NULL)
+            fprintf(stderr, "norm_dict: %s\n", msg);
+        fprintf(stderr, "Try `%s -h' for more information.\n", program_name);
+        exit(code);
+    }
+
+    printf("%s\n", program_name);
+    printf("Normalize the plain text dictionary.\n");
+    printf("Usage: %s [options] [input file]\n", program_name);
+    printf("  -i        Specified the plain text dictionary(default: dict.txt).\n");
+    printf("  -v        Show the version.\n");
+    printf("  -h        Show this page.\n");
+    exit(0);
+}
 
 int
-main(void)
+main(int argc, char *argv[])
 {
-    printf("Hello norm!\n");
-    return 0;
+    int c, t;
+    char *input, *delim = " \t\r\n";
+    FILE *fp;
+    char buf[256], attr[256], *str, *ptr;
+    float tf, idf;
+
+    input = NULL;
+    if ((program_name = strrchr(argv[0], '/')) != NULL)
+        program_name++;
+    else
+        program_name = argv[0];	
+
+    /* parse the arguments */
+    while ((c = getopt(argc, argv, "i:vh")) != -1)
+    {
+        switch (c)
+        {
+            case 'i' :
+                input = optarg;
+                break;
+            case 'v' :
+                printf("%s: convert the plain text dictionary to xdb format\n",	program_name);
+                exit(0);			
+                break;
+            case 'h' :
+                show_usage(0, NULL);
+                break;
+            case '?' :
+                default :
+                exit(-1);
+        }
+    }
+
+    /* other arguments */
+    argc -= optind;
+    if (argc > 0 && input == NULL)
+    {
+        input = argv[optind++];		
+        argc--;
+    }
+
+    if (input == NULL)
+        input = "dict.txt";
+
+    /* check the input */
+    if ((fp = fopen(input, "r")) == NULL)
+    {
+        perror("Cann't open the input file");
+        return -1;
+    }
+
+    /* load the data */
+    //printf("Reading the input file: %s\n", input);
+    //fflush(stdout);
+
+    t = 0;
+    while (fgets(buf, sizeof(buf) - 1, fp) != NULL)
+    {
+        // <word>\t<tf>\t<idf>\t<attr>\n
+        if (buf[0] == ';' || buf[0] == '#')
+        {
+            printf("%s\n", buf);
+            continue;
+        }
+
+        str = strtok(buf, delim);
+        if (str == NULL)
+            continue;
+        c = strlen(str);
+		
+        // init the word
+        tf = idf = 1.0;
+
+        if (!(ptr = strtok(NULL, delim)))
+            break;
+        tf = (float) atof(ptr);
+
+        if (!(ptr = strtok(NULL, delim)))
+            break;
+        idf = (float) atof(ptr);
+
+        if (ptr = strtok(NULL, delim))
+	    strcpy(attr, ptr);
+
+	//printf("word: %s (len=%d)\n", str, c);
+	t++;
+    }
+
+    //printf("Total nodes=%d\n", t);
+    //fflush(stdout);
+	
+    fclose(fp);
+    return 0;	
 }
